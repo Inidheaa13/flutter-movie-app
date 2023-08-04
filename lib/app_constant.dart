845ece39a74141b3725542aecef04bebd63869fa
Inidheaa13 <127503050+Inidheaa13@users.noreply.github.com>
2023-08-04 09:22:27 +0700

    initial and setup project setup splash screen

---
+++ lib/app_constant.dart
@@ -0,0 +1,6 @@
+class AppConstant {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlW500 = 'https://image.tmdb.org/t/p/w500';
+  static const String imageUrlOriginal = 'https://image.tmdb.org/t/p/original';
+  static const String apiKey = '2248f1cf5d88d908afbe18b329042778';
+}
