a69d3ca2998f04cd22f51edf8fdeb2978f467f08
Inidheaa13 <127503050+Inidheaa13@users.noreply.github.com>
2023-08-09 13:38:22 +0700

    create widget discover movies

--- lib/movie/pages/movie_page.dart
+++ lib/movie/pages/movie_page.dart
@@ -1,44 +1,226 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/movie/models/movie_model.dart';
 import 'package:flutter_application_1/movie/providers/movie_get_discover_provider.dart';
+import 'package:flutter_application_1/widget/image_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_application_1/app_constant.dart';
 
 class MoviePage extends StatelessWidget {
   const MoviePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    child: Image.asset(
+                      'assets/images/logo1.png',
+                    ),
+                  ),
+                ),
+                Text('MOVIEW'),
+              ],
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Discover Movies',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      shape: StadiumBorder(),
+                      side: BorderSide(
+                        color: Colors.black54,
+                      ),
+                    ),
+                    child: Text('See All'),
+                  )
+                ],
+              ),
+            ),
+          ),
+          WidgetDiscoverMovie(),
+        ],
+      ),
+    );
+  }
+}
+
+class WidgetDiscoverMovie extends StatefulWidget {
+  const WidgetDiscoverMovie({super.key});
+
+  @override
+  State<WidgetDiscoverMovie> createState() => _WidgetDiscoverMovieState();
+}
+
+class _WidgetDiscoverMovieState extends State<WidgetDiscoverMovie> {
+  @override
+  void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<MovieGetDiscoverProvider>().getDiscover(context);
     });
+    super.initState();
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MOVIEW'),
-      ),
-      body: Consumer<MovieGetDiscoverProvider>(
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Consumer<MovieGetDiscoverProvider>(
         builder: (_, provider, __) {
           if (provider.isLoading) {
-            return Center(
-              child: CircularProgressIndicator(),
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.circular(12),
+              ),
             );
           }
           if (provider.movies.isNotEmpty) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
+            return CarouselSlider.builder(
+              itemCount: provider.movies.length,
+              itemBuilder: (_, index, __) {
                 final movie = provider.movies[index];
-
-                return ListTile(
-                  title: Text(movie.title),
-                  subtitle: Text(movie.overview),
-                );
+                return ItemMovie(movie);
               },
+              options: CarouselOptions(
+                height: 300.0,
+                viewportFraction: 0.8,
+                reverse: false,
+                autoPlay: true,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+              ),
             );
           }
-          return Center(
-            child: Text('Not Found Discover Movies'),
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 16.0),
+            height: 300,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black26,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Center(
+              child: Text(
+                'Not Found Discover Movie',
+                style: TextStyle(
+                  color: Colors.black45,
+                ),
+              ),
+            ),
           );
         },
       ),
     );
   }
+}
+
+class ItemMovie extends Container {
+  final MovieModel movie;
+
+  ItemMovie(this.movie, {super.key});
+
+  @override
+  Clip get clipBehavior => Clip.hardEdge;
+
+  @override
+  Decoration? get decoration => BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+      );
+
+  @override
+  Widget? get child => Stack(
+        children: [
+          ImageNetworkWidget(
+            imageSrc: '${movie.backdropPath}',
+            height: 300.0,
+            width: double.infinity,
+          ),
+          Container(
+            height: 300.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Colors.black87,
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 16.0,
+            left: 16.0,
+            right: 16.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ImageNetworkWidget(
+                  imageSrc: '${movie.posterPath}',
+                  height: 160,
+                  width: 100,
+                  radius: 10,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  movie.title,
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.amber,
+                    ),
+                    Text(
+                      '${movie.voteAverage} (${movie.voteCount})',
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
 }
